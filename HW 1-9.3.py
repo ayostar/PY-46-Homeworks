e7c43fbee9eb3b30aa110b8ceda85d8a402d446a
Artem Starodubtsev <ayostar@gmail.com>
2021-10-26 11:17:34 +0300

    Сделал вывод через параметр Total

--- HW 1-9.3.py
+++ HW 1-9.3.py
@@ -7,22 +7,23 @@ import requests as req
 class Stackoverflow:
 
     def get_questions_wth_tag(self, tag, days):
-        url = "https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow"
+        url = "https://api.stackexchange.com/2.3/questions"
         dt_temp = datetime.now()
         days = timedelta(days)
         from_date_temp = dt_temp - days
         from_date = from_date_temp.strftime('%Y-%m-%d')
         to_date = dt_temp.strftime('%Y-%m-%d')
         headers = {'Accept': 'application/json'}
-        # params = {'tagged': tag, 'fromdate': from_date}
-        params = {'fromdate': from_date, 'todate': to_date, 'tagged': tag}
+        params = {'order': 'desc', 'sort': 'activity', 'site': 'stackoverflow', 'fromdate': from_date, 'todate': to_date, 'tagged': tag, 'filter': 'total'}
         response = req.get(url=url, headers=headers, params=params)
         response_dict = dict(response.json())
-        list_of_questions = response_dict.get('items')
+        total_number_of_questions = response_dict.get('total')
+        return f'Количество запросов с тэгом {tag} составляет: {total_number_of_questions}'
+        # list_of_questions = response_dict.get('items')
         # for el in list_of_questions:
         #     print(el.get('tags'))
-        number_of_questions = len(list_of_questions)
-        return f'Количество запросов с тэгом {tag} составляет: {number_of_questions}'
+        # number_of_questions = len(list_of_questions)
+        # return f'Количество запросов с тэгом {tag} составляет: {number_of_questions}'
 
 
 if __name__ == '__main__':
