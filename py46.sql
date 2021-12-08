6055229090b1b38fa8efaba99d636d8a23b5a276
Artem Starodubtsev <ayostar@gmail.com>
2021-12-08 14:54:57 +0300

    Поправлен скрипт HW 2-2 (py46.sql)

--- py46.sql
+++ py46.sql
@@ -1,18 +1,18 @@
-create table if not exists Singer (
-ID serial primary key,
-Name varchar(40) not null unique
+create table if not exists singer (
+id serial primary key,
+name varchar(40) not null unique
 );
 
-create table if not exists Album (
-ID serial primary key,
-Name varchar(40) not null unique,
-Singer integer references singer(ID),
-year date
+create table if not exists album (
+id serial primary key,
+name varchar(40) not null,
+singer integer references singer(ID),
+year date not null
 );
 
-create table if not exists Track(
-ID serial primary key,
-Album integer references album(ID),
-Name varchar(40),
-Length numeric(4,2)
+create table if not exists track(
+id serial primary key,
+album integer references album(ID),
+name varchar(40),
+length numeric(4,2) CHECK (length > 0)
 );
