7ccad9817dfe8a1a5e81ca5eccfb3ab94ae7f3b3
Artem Starodubtsev <ayostar@gmail.com>
2021-12-14 11:19:59 +0300

    HW 2-3

---
+++ HW 2-3-2 py46.sql
@@ -0,0 +1,46 @@
+create table if not exists singer (
+id serial primary key,
+name varchar(40) not null unique
+);
+
+create table if not exists album (
+id serial primary key,
+name varchar(40) not null,
+year integer not null
+);
+
+create table if not exists track (
+id serial primary key,
+album integer references album(id),
+name varchar(40),
+length numeric(4,2) CHECK (length > 0)
+);
+
+create table if not exists style (
+id serial primary key,
+name varchar(40) not null unique
+);
+
+
+create table if not exists collection (
+id serial primary key,
+name varchar(40),
+year integer not null
+);
+
+create table if not exists singer_style (
+singer_id integer references singer(id),
+style_id integer references style(id)
+);
+
+create table if not exists singer_album (
+id serial primary key,
+singer_id integer references singer(id),
+album_id integer references album(id)
+);
+
+create table if not exists track_collection (
+id serial primary key,
+track_id integer references track(id),
+collection_id integer references collection(id)
+);
---
+++ py46 lector adjusted.sql
@@ -0,0 +1,18 @@
+create table if not exists singer (
+id serial primary key,
+name varchar(40) not null unique
+);
+
+create table if not exists album (
+id serial primary key,
+name varchar(40) not null,
+singer integer references singer(id),
+year integer not null
+);
+
+create table if not exists track(
+id serial primary key,
+album integer references album(id),
+name varchar(40),
+length numeric(4,2) CHECK (length > 0)
+);
